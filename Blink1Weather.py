e370fb0ea861e459f02919fa1b01b9ae55b0b4a6
Marcello Nicoletti <marcello.s.nicoletti@gmail.com>
2015-02-25 20:17:08 -0700

    Remove extra json parse
    also renamed lots of variables

--- Blink1Weather.py
+++ Blink1Weather.py
@@ -10,23 +10,26 @@ from subprocess import call
 
 # Variables
 localUnknownTypesFile = "/path/to/unknownTypes/file"
-url = "http://api.openweathermap.org/data/2.5/weather?id=0000&APPID=11111"
-red = 0
-green = 0
-blue = 0
-nightBrightnessFactor = 4 # Output is divided by this number at night
+weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=0000&APPID=11111"
+
+redSetting = 0
+greenSetting = 0
+blueSetting = 0
+
+NIGHT_BRIGHTNESS_DIVISOR = 4 # Color settings are divided by this number at night
 
 # Parses API call to python dict
-webFile = str(urllib.request.urlopen(url).read())[2:-3]
-jsonDict = json.loads(webFile)
+webFile = str(urllib.request.urlopen(weatherApiUrl).read())[2:-3]
+webFile = webFile.replace("[","").replace("]","") # Clean up the extra array around the weather object
+jsonObject = json.loads(webFile)
 
-# Parses a piece of the the API call dict into its own dict
-weatherDict = json.loads(str(jsonDict["weather"][0]).replace("\'","\""))
-condition = str(weatherDict["main"])
+condition = str(jsonObject["weather"]["main"])
+print("Condition is: " + condition)
 
 # Parse sun up and sun down times and function for if sun is up
-sunRiseTime = jsonDict["sys"]["sunrise"]
-sunSetTime = jsonDict["sys"]["sunset"]
+sunRiseTime = jsonObject["sys"]["sunrise"]
+sunSetTime = jsonObject["sys"]["sunset"]
+
 def sunIsUp():
 	currentTime = time.time()
 	if (sunRiseTime <= currentTime) and (currentTime <= sunSetTime):
@@ -34,52 +37,52 @@ def sunIsUp():
 	else:
 		return False
 
-# I didn't know what conditions were going to be passed by the API, so I have the script record all unknown contition types to a file.
+# I didn't know what conditions were going to be passed by the API, so I have the script record all unknown condition types to a file.
 # After running for 2 weeks I had enough types to see that the conditions I was parsing matched the group names (in bold) on [this](http://openweathermap.org/weather-conditions) list.
-# I went ahead and prepopulated some of the conditions I hadn't parsed yet from that list, they are marked.
-# Unfortunatly, some of the stuff that should be under the atmosphere group show up as their own main condition.
+# I went ahead and pre-populated some of the conditions I hadn't parsed yet from that list, they are marked.
+# Unfortunately, some of the stuff that should be under the atmosphere group show up as their own main condition.
 # Therefore I'm keeping the recording functionality to catch any other oddities like this.
 
 if (condition == "Atmosphere") or (condition == "Fog") or (condition == "Haze") or (condition == "Mist"): # Haze and Mist should have shown up as Atmosphere, I don't know why they didnt't, Atmosphere is from the list.
 	# Yellow
-	red = 255
-	green = 255
-	blue = 0
+	redSetting = 255
+	greenSetting = 255
+	blueSetting = 0
 elif (condition == "Clear"):
 	# Green
-	red = 0
-	green = 255
-	blue = 0
+	redSetting = 0
+	greenSetting = 255
+	blueSetting = 0
 elif (condition == "Clouds"):
-	# Yellow-ish Green Not very distinct from green, but clouds and clear are close enough for me that I don't care.
-	red = 170
-	green = 255
-	blue = 0
+	# Yellow-ish Green Not very distinct from Green, but clouds and clear are close enough for me that I don't care.
+	redSetting = 170
+	greenSetting = 255
+	blueSetting = 0
 elif (condition == "Drizzle"): # From List
 	# Cyan
-	red = 0
-	green = 255
-	blue = 255	
+	redSetting = 0
+	greenSetting = 255
+	blueSetting = 255	
 elif (condition == "Extreme"): # From List
 	# Red
-	red = 255
-	green = 0
-	blue = 0
+	redSetting = 255
+	greenSetting = 0
+	blueSetting = 0
 elif (condition == "Rain"): # From List
 	# Blue
-	red = 0
-	green = 0
-	blue = 255
+	redSetting = 0
+	greenSetting = 0
+	blueSetting = 255
 elif (condition == "Snow"):
 	# White
-	red = 255
-	green = 255
-	blue = 255
+	redSetting = 255
+	greenSetting = 255
+	blueSetting = 255
 elif (condition == "Thunderstorm"): # From list
 	# Magenta
-	red = 255
-	green = 0
-	blue = 255
+	redSetting = 255
+	greenSetting = 0
+	blueSetting = 255
 else:
 	# For adding unknown types to the unknown types file
 	typeExistsInUnknownTypesFile = False
@@ -95,9 +98,9 @@ else:
 
 # If the sun isn't up I want the colors to be less bright
 if not sunIsUp():
-	red = red / nightBrightnessFactor
-	green = green / nightBrightnessFactor
-	blue = blue / nightBrightnessFactor
+	redSetting = redSetting / NIGHT_BRIGHTNESS_DIVISOR
+	greenSetting = greenSetting / NIGHT_BRIGHTNESS_DIVISOR
+	blueSetting = blueSetting / NIGHT_BRIGHTNESS_DIVISOR
 
 # Blink1-tool call, colors set above, defaults to off.
-call(["sudo", "blink1-tool", "--rgb", str(red) + "," + str(green) + "," + str(blue)])
+call(["sudo", "blink1-tool", "--rgb", str(redSetting) + "," + str(greenSetting) + "," + str(blueSetting)])
